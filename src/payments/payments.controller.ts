90babcc22e109df17f6dd10beb718c4de64719aa
jpena <juan.pena@wmcontractor.com>
2025-01-20 15:09:44 -0500

    New paymentSession and webhook Implementation

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,37 +1,36 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { PaymentSessionDto } from './dto/payment-session.tdo';
+import { Request, Response } from 'express';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-    @Post('create-payment-session')
-    createPaymentSession() {
-      return this.paymentsService.createPaymentSession();
-    }
+  @Post('create-payment-session')
+  createPaymentSession(@Body() paymentSessionDto: PaymentSessionDto) {
+    return this.paymentsService.createPaymentSession(paymentSessionDto);
+  }
 
-    @Get('success')
-    success() {
-      return {
-        ok: true,
-        message: 'Payment successful'
-      }
-    }
+  @Get('success')
+  success() {
+    return {
+      ok: true,
+      message: 'Payment successful',
+    };
+  }
 
-    @Get('cancel')
-    cancel() {
-      return {
-        ok: false,
-        message: 'Payment cancelled'
-      }
-    }
+  @Get('cancel')
+  cancel() {
+    return {
+      ok: false,
+      message: 'Payment cancelled',
+    };
+  }
 
-    @Post('webhook')
-    async stripeWebhook() {
-      return 'stripeWebhook'
-    }
-
-
-
-
+  @Post('webhook')
+  async stripeWebhook(@Req() req: Request, @Res() res: Response) {
+    console.log('Stripe called');
+    return this.paymentsService.stripeWebhook(req, res);
+  }
 }
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,34 +1,76 @@
 import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config/envs';
 import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.tdo';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
-  async createPaymentSession() {
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items, orderId } = paymentSessionDto;
+
+    const lineItems = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.round(item.price * 100),
+        },
+        quantity: item.quantity,
+      };
+    });
     const session = await this.stripe.checkout.sessions.create({
       // Put here ID order
       payment_intent_data: {
-        metadata: {},
-      },
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-Shirt',
-            },
-            unit_amount: 2000,
-          },
-          quantity: 2,
+        metadata: {
+          orderId: orderId,
         },
-      ],
+      },
+      line_items: lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:3003/payments/success',
-      cancel_url: 'http://loclahost:3033/payments/cancel',
+      success_url: envs.successUrl,
+      cancel_url: envs.cancelUrl,
     });
 
     return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    const endpointSecret = envs.stripeEndpointSecret;
+
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
+    } catch (error) {
+      res.status(400).send(`Webhook Error: ${error.message}`);
+    }
+
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceeded = event.data.object;
+
+        console.log({
+          metadata: chargeSucceeded.metadata,
+        });
+
+        console.log({ event });
+        break;
+
+      default:
+        break;
+    }
+
+    return res.status(200).json({ sig });
   }
 }
